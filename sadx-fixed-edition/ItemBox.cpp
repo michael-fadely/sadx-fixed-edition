3e734b79433791707d4d1e17c40dede44cbf03f4
Michael Fadely <michaelwfadely@gmail.com>
2016-12-05 08:58:22 -0700

    Re-implemented item box rotation and restored lighting

--- sadx-fixed-edition/ItemBox.cpp
+++ sadx-fixed-edition/ItemBox.cpp
@@ -2,149 +2,151 @@
 #include <SADXModLoader.h>
 
 DataPointer(int, WhatIsThisAndWhyIsItAlmostAlwaysZero, 0x3B1117C);
+DataPointer(int, SomeDepthThing, 0x03ABD9C0);
 
-DataPointer(NJS_MODEL, ItemBox_Base_MODEL, 0x8BF260);
-DataPointer(NJS_MODEL, stru_8BE270, 0x8BE270);
-DataPointer(float, dword_3ABD9C0, 0x3ABD9C0);
-DataPointer(NJS_MODEL, stru_8BEAA8, 0x8BEAA8);
-DataPointer(NJS_MODEL, stru_8BE620, 0x8BE620);
+DataPointer(NJS_OBJECT, ItemBox_Base_OBJECT, 0x008BF28C);
+DataPointer(NJS_MODEL_SADX, ItemBox_Base_MODEL, 0x008BF260);
+DataArray(NJS_MATERIAL, mat_008BEB08, 0x008BEB08, 4);
+DataArray(NJS_MESHSET_SADX, mesh_008BEE40, 0x008BEE40, 5);
+DataPointer(NJS_OBJECT, ItemBox_Capsule_OBJECT, 0x008BEAD4);
+DataPointer(NJS_MODEL_SADX, ItemBox_Capsule_MODEL, 0x008BEAA8);
+DataArray(NJS_MATERIAL, mat_008BE680, 0x008BE680, 1);
+DataArray(NJS_MESHSET_SADX, mesh_008BE848, 0x008BE848, 1);
+DataPointer(NJS_OBJECT, ItemBox_Top_OBJECT, 0x008BE64C);
+DataPointer(NJS_MODEL_SADX, ItemBox_Top_MODEL, 0x008BE620);
+DataArray(NJS_MATERIAL, mat_008BE2D0, 0x008BE2D0, 2);
+DataArray(NJS_MESHSET_SADX, mesh_008BE478, 0x008BE478, 3);
+DataPointer(NJS_OBJECT, ItemBox_Item_OBJECT, 0x008BE29C);
+DataPointer(NJS_MODEL_SADX, ItemBox_Item_MODEL, 0x008BE270);
+DataArray(NJS_MATERIAL, mat_008BE1C8, 0x008BE1C8, 1);
+DataArray(NJS_MESHSET_SADX, mesh_008BE1F4, 0x008BE1F4, 1);
 
-// TODO: Do this with code patches instead.
+// TODO: Use trampolines
 
-void __cdecl ItemBox_Display_Destroyed_Rotate(ObjectMaster* thing)
+void __cdecl ItemBox_Display_Destroyed_Rotate(ObjectMaster* _this)
 {
-	//PrintDebug("ItemBox_Display_Destroyed\n");
-
-	// esi@1
-	auto entity = thing->Data1;
+	auto v1 = _this->Data1;
 	SetCurrentTextureToCommon();
 	njPushMatrix(nullptr);
-	njTranslateV(nullptr, &entity->Position);
-	njRotateZYX(nullptr, entity->Rotation.x, (ControllerPointers[0]->HeldButtons & Buttons_C) ? entity->Rotation.y : 0, entity->Rotation.z);
+	njTranslateV(nullptr, &v1->Position);
+
+	// Rotate
+	njRotateEx((Angle*)&v1->Rotation, 0);
 
 	DrawModelIGuess(&ItemBox_Base_MODEL);
-	njPopMatrix(1);
+	njPopMatrix(1u);
 }
 
-void __cdecl ItemBox_Display_Unknown_Rotate(ObjectMaster* thing)
+void __cdecl ItemBox_Display_Unknown_Rotate(ObjectMaster* _this)
 {
-	// esi@1
-	auto entity = thing->Data1;
+	auto v1 = _this->Data1;
 	if (!WhatIsThisAndWhyIsItAlmostAlwaysZero)
 	{
 		ItemBoxPowerups[6].Texture = MetalSonicFlag ? 98 : LifeTextures[GetCurrentCharacterID()];
-
-		if (CheckModelDistance_v(&entity->Position, 20.0))
+		if (IsVisible(&v1->Position, 20.0f))
 		{
 			SetCurrentTextureToCommon();
-			PushMatrix();
+			njPushMatrixEx();
 
-			// ebx@6
-			auto attach = (NJS_MODEL*)late_alloca(44);
+			auto model = (NJS_MODEL_SADX *)late_alloca(44);
+			auto material = (NJS_MATERIAL *)late_alloca(20);
 
-			// eax@6
-			auto material = (NJS_MATERIAL*)late_alloca(20);
-
-			if (attach && material)
+			if (model && material)
 			{
-				njTranslateEx(&entity->Position);
-				njRotateZYX(nullptr, entity->Rotation.x, (ControllerPointers[0]->HeldButtons & Buttons_C) ? entity->Rotation.y : 0, entity->Rotation.z);
+				njTranslateEx(&v1->Position);
 
-				if (entity->Action != 2)
+				// Rotate
+				njRotateEx((Angle*)&v1->Rotation, 0);
+
+				if (v1->Action != 2)
 				{
-					// ST28_4@9
-					float scale = entity->Scale.z * 0.2000000029802322f;
+					auto scale = v1->Scale.z * 0.2f;
 					njScale(nullptr, scale, scale, scale);
 				}
+				njPushMatrixEx();
+				njTranslate(nullptr, 0.0f, 7.5f, 0.0f);
+				auto v6 = (Uint16)(v1->Scale.y * 65536.0f * 0.002777777777777778f);
+				if (v6)
+				{
+					njRotateY(nullptr, v6);
+				}
+				memcpy(model, &ItemBox_Item_MODEL, 0x2Cu);
+				memcpy(material, ItemBox_Item_MODEL.mats, 0x14u);
+				model->mats = material;
+				auto texId = ItemBoxPowerups[(int)_this->Data1->Scale.x].Texture;
+				ItemBoxPowerups_Index = (int)_this->Data1->Scale.x;
+				material->attr_texId = texId;
+				DrawModelIGuess(model);
+				njPopMatrixEx();
+				SomeDepthThing = 0xC68C4000;
 
-				PushMatrix();
-				njTranslate(nullptr, 0.0, 7.5, 0.0);
+				// This was originally DrawModelIGuess_N, but that's wrong.
+				DrawModelIGuess(&ItemBox_Base_MODEL);
 
-				// st7@10
-				double v6 = entity->Scale.y * 65536.0 * 0.002777777777777778;
-				if ((unsigned int)(unsigned __int64)v6)
-					njRotateY(nullptr, (unsigned __int16)(unsigned __int64)v6);
+				DrawModelIGuess_N(&ItemBox_Capsule_MODEL, 5);
 
-				memcpy(attach, &stru_8BE270, 0x2Cu);
-				memcpy(material, stru_8BE270.mats, 0x14u);
+				// This was originally DrawModelIGuess_N, but that's wrong.
+				DrawModelIGuess(&ItemBox_Top_MODEL);
 
-				attach->mats = material;
-				// ecx@12
-				int v7 = ItemBoxPowerups[(unsigned __int64)thing->Data1->Scale.x].Texture;
-				ItemBoxPowerups_Index = (unsigned __int64)thing->Data1->Scale.x;
-				material->attr_texId = v7;
-				DrawModelIGuess(attach);
-				PopMatrix();
-
-				dword_3ABD9C0 = -17952;
-				DrawModelIGuess_N(&ItemBox_Base_MODEL, 5);
-				DrawModelIGuess_N(&stru_8BEAA8, 5);
-				DrawModelIGuess_N(&stru_8BE620, 5);
-				dword_3ABD9C0 = 0;
+				SomeDepthThing = 0;
 			}
-
-			PopMatrix();
+			njPopMatrixEx();
 		}
 	}
 }
 
-void __cdecl ItemBox_Display_Rotate(ObjectMaster* thing)
+void __cdecl ItemBox_Display_Rotate(ObjectMaster* _this)
 {
-	//PrintDebug("ItemBox_Display\n");
-
+	auto v1 = _this->Data1;
 	if (!WhatIsThisAndWhyIsItAlmostAlwaysZero)
 	{
 		ItemBoxPowerups[6].Texture = MetalSonicFlag ? 98 : LifeTextures[GetCurrentCharacterID()];
-
-		// esi@1
-		auto entity = thing->Data1;
-		if (CheckModelDistance_v(&entity->Position, 20.0))
+		if (IsVisible(&v1->Position, 20.0f))
 		{
 			SetCurrentTextureToCommon();
 			njPushMatrix(nullptr);
-			njTranslateV(nullptr, &entity->Position);
-			njRotateZYX(nullptr, entity->Rotation.x, (ControllerPointers[0]->HeldButtons & Buttons_C) ? entity->Rotation.y : 0, entity->Rotation.z);
+			njTranslateV(nullptr, &v1->Position);
 
-			if (entity->Action != 2)
+			// Rotate
+			njRotateEx((Angle*)&v1->Rotation, 0);
+
+			if (v1->Action != 2)
 			{
-				// ST28_4@7
-				float v2 = entity->Scale.z * 0.2000000029802322f;
-				njScale(nullptr, v2, v2, v2);
+				auto scale = v1->Scale.z * 0.2f;
+				njScale(nullptr, scale, scale, scale);
 			}
+			auto model = (NJS_MODEL_SADX *)late_alloca(44);
+			auto material = (NJS_MATERIAL *)late_alloca(20);
 
-			// ebx@8
-			auto attach = (NJS_MODEL*)late_alloca(44);
-			// eax@8
-			auto material = (NJS_MATERIAL*)late_alloca(20);
-
-			if (attach && material)
+			if (model && material)
 			{
-				PushMatrix();
-				njTranslate(nullptr, 0.0, 7.5, 0.0);
+				njPushMatrixEx();
+				njTranslate(nullptr, 0.0f, 7.5f, 0.0f);
+				auto v6 = (Uint16)(v1->Scale.y * 65536.0f * 0.002777777777777778f);
+				if (v6)
+				{
+					njRotateY(nullptr, v6);
+				}
+				memcpy(model, &ItemBox_Item_MODEL, 0x2Cu);
+				memcpy(material, ItemBox_Item_MODEL.mats, 0x14u);
 
-				// st7@10
-				double v6 = entity->Scale.y * 65536.0 * 0.002777777777777778;
-				if ((unsigned int)(unsigned __int64)v6)
-					njRotateY(nullptr, (unsigned __int16)(unsigned __int64)v6);
-
-				memcpy(attach, &stru_8BE270, 0x2Cu);
-				memcpy(material, stru_8BE270.mats, 0x14u);
-				attach->mats = material;
-
-				// ecx@12
-				int v7 = ItemBoxPowerups[(unsigned __int64)thing->Data1->Scale.x].Texture;
-				ItemBoxPowerups_Index = (unsigned __int64)thing->Data1->Scale.x;
-
+				model->mats = material;
+				auto v7 = ItemBoxPowerups[(int)_this->Data1->Scale.x].Texture;
+				ItemBoxPowerups_Index = (int)_this->Data1->Scale.x;
 				material->attr_texId = v7;
-				DrawModelIGuess(attach);
-				PopMatrix();
 
-				DrawModelIGuess_N(&ItemBox_Base_MODEL, 5);
-				DrawModelIGuess_N(&stru_8BEAA8, 5);
-				DrawModelIGuess_N(&stru_8BE620, 5);
+				DrawModelIGuess(model);
+				njPopMatrixEx();
+
+				// This was originally DrawModelIGuess_N, but that's wrong.
+				DrawModelIGuess(&ItemBox_Base_MODEL);
+
+				DrawModelIGuess_N(&ItemBox_Capsule_MODEL, 5);
+
+				// This was originally DrawModelIGuess_N, but that's wrong.
+				DrawModelIGuess(&ItemBox_Top_MODEL);
 			}
-
-			PopMatrix();
+			njPopMatrixEx();
 		}
 	}
 }
