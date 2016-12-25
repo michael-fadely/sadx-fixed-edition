8bc22cc4090490208f958a0a851026e88e6b5922
Michael Fadely <michaelwfadely@gmail.com>
2016-12-25 15:42:54 -0700

    Implemented Speed Highway platform fix. Thanks PkR!

--- sadx-fixed-edition/mod.cpp
+++ sadx-fixed-edition/mod.cpp
@@ -1,11 +1,17 @@
 #include "stdafx.h"
 
+// Because ain't nobody got time for compiler warnings
+#define _arrayptrandlength(data) data, (int)LengthOfArray(data)
+#define _arraylengthandptr(data) (int)LengthOfArray(data), data
+
 #define EXPORT __declspec(dllexport)
 
 #include <SADXModLoader.h>
 
 #include "ItemBox.h"
 #include "PlaySegaSonicTeamVoice.h"
+
+DataArray(NJS_MATERIAL, matlist_022710E0, 0x026710E0, 5);
 
 // Replaces:	mov	camerathing,	80000004h
 // With:		or	camerathing,	80000004h
@@ -17,11 +23,11 @@ static Uint32 CasinoSpawnY = 0xC3480001;	// Secretly a float of about -200.0
 
 static PatchInfo patches[] = {
 	// MT_KUSA. Also found at 0x0082F216 in SADX 2010
-	{ (void*)(0x00608380 + 0x1D), arrayptrandlength(mt_kusa_nop) },
+	{ (void*)(0x00608380 + 0x1D), _arrayptrandlength(mt_kusa_nop) },
 	// Action stage load and restart
-	{ (void*)0x00438330, arrayptrandlength(freecam_fix) },
+	{ (void*)0x00438330, _arrayptrandlength(freecam_fix) },
 	// Adventure field scene change
-	{ (void*)0x00434870, arrayptrandlength(freecam_fix) },
+	{ (void*)0x00434870, _arrayptrandlength(freecam_fix) },
 	// Casinopolis spawn point Y offset fix
 	{ (void*)0x005C0D5D, &CasinoSpawnY, sizeof(float) }
 };
@@ -37,8 +43,8 @@ static PointerInfo jumps[] = {
 
 extern "C"
 {
-	EXPORT PatchList Patches[] = { { arrayptrandlength(patches) } };
-	EXPORT PointerList Jumps[] = { { arrayptrandlength(jumps) } };
+	EXPORT PatchList Patches[] = { { _arrayptrandlength(patches) } };
+	EXPORT PointerList Jumps[] = { { _arrayptrandlength(jumps) } };
 	EXPORT ModInfo SADXModInfo = { ModLoaderVer };
 
 	EXPORT void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
@@ -59,14 +65,18 @@ extern "C"
 		obj->pos[1] = -3.0f;
 		obj->pos[2] = -5850.0f;
 
+		// Fixes a rendering issue with the red moving platform in Speed Highway.
+		// Thanks PkR!
+		matlist_022710E0[0].attrflags &= ~NJD_FLAG_USE_ALPHA;
+
 		// Leon fix
 		WriteData((Float**)0x004CD75A, &_nj_screen_.w);
 		WriteData((Float**)0x004CD77C, &_nj_screen_.h);
 
-		PlaySegaSonicTeamVoice_init();
-
 		// Fixes bounds for Sky Chase target reticle
 		WriteData((Float**)0x00628994, &HorizontalStretch);
 		WriteData((Float**)0x00628A13, &VerticalStretch);
+
+		PlaySegaSonicTeamVoice_init();
 	}
 }
