241afd160797d7501ba4c2013dd210968f8e1107
Michael Fadely <michaelwfadely@gmail.com>
2017-05-13 07:27:44 -0600

    Move preprocessor macros to stdafx.h

--- sadx-fixed-edition/mod.cpp
+++ sadx-fixed-edition/mod.cpp
@@ -1,11 +1,5 @@
 #include "stdafx.h"
 
-// Because ain't nobody got time for compiler warnings
-#define _arrayptrandlength(data) data, (int)LengthOfArray(data)
-#define _arraylengthandptr(data) (int)LengthOfArray(data), data
-
-#define EXPORT __declspec(dllexport)
-
 #include <SADXModLoader.h>
 
 #include "ItemBox.h"
